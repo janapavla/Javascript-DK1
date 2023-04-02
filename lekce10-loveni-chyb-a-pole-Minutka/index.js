c24dbdebe6cebc909118f6251f0457654d2c3f24
janapavla <janinka-a@seznam.cz>
2023-04-02 19:30:30 +0200

    ukol 10

--- lekce10-loveni-chyb-a-pole-Minutka/index.js
+++ lekce10-loveni-chyb-a-pole-Minutka/index.js
@@ -1,30 +1,29 @@
 const setAlarm = (event) => {
     event.preventDefault()
-    let timeInput = Number(document.querySelector('.time-input').value)
+    let timeInput = Number(document.querySelector(".time-input").value)
 
     if (Number.isInteger(timeInput) && (timeInput > 0) && (timeInput !== null)) {
-        const alarmSeconds = document.querySelector('.alarm__seconds')
-        const alarm = document.querySelector('.alarm')
-        const audio = document.querySelector('.audio')
-        alarmSeconds.textContent = String(timeInput).padStart(2, '0')
+        const alarmSeconds = document.querySelector(".alarm__seconds")
+        const alarm = document.querySelector(".alarm")
+        const audio = document.querySelector(".audio")
+        alarmSeconds.textContent = String(timeInput).padStart(2, "0")
 
         const deduct = () => {
             if (timeInput > 0) {
                 timeInput = (timeInput - 1)
-                let seconds = timeInput
-                alarmSeconds.textContent = String(seconds).padStart(2, '0')
+                alarmSeconds.textContent = String(timeInput).padStart(2, "0")
             } else if (timeInput <= 0) {
                 clearInterval(timerId)
-                alarm.classList.add('alarm--ring')
-                document.querySelector('audio').play()
+                alarm.classList.add("alarm--ring")
+                document.querySelector("audio").play()
             }
         }
         const timerId = setInterval(deduct, 1000)
 
     } else {
-        alert('Zadej celé kladné číslo!')
+        alert("Zadej celé kladné číslo!")
     }
 }
 
-const formular = document.querySelector('.controls')
-formular.addEventListener('submit', (event) => setAlarm(event))
+const formular = document.querySelector(".controls")
+formular.addEventListener("submit", (event) => setAlarm(event))
